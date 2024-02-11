d527a404a68b9b0a684fa1e830c11c532c94393c
cait0703 <cait.lanning@gmail.com>
2024-02-11 13:34:17 -0800

    update to assignment

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function displayMessage() {
-    alert('Hello World')
+/*
+    Project: Lab 01: My First JavaScript Application
+    Date: 2/11/2024
+    Author: Cait Lanning
+*/
+
+// Declare the variable and collect a number from the user
+let num = prompt('Guess a number between 1 and 10:');
+// Check and see if they entered the correct number
+if (num == 3) {
+    // Display message saying they won
+    alert('You guessed the right number!');
+} else {
+    // Display message saying they lost
+    alert('You guessed the wrong number!');
 }
